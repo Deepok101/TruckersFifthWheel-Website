14915c14904dff9c7e5b1b960002c911aef89932
Deepak Singh <deepakanhviet@gmail.com>
2019-05-29 22:46:55 -0400

    Initing

--- src/components/chat.js
+++ src/components/chat.js
@@ -9,7 +9,7 @@ class Chat extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            endpoint: 'https://still-taiga-69176.herokuapp.com:52416/',
+            endpoint: 'https://still-taiga-69176.herokuapp.com:80',
             message: "",
             allmsg: [],
             htmlmsg: null
